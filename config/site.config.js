73b4ee559b249c7297288b25ad8a519c12cd45bf
unbengable <51025675+unbengable@users.noreply.github.com>
2023-04-12 18:36:24 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: '喵呜',
+      link: '996.icu',
     },
   ],
 
